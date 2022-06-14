428be3cace585d81212a7740b0caee60576d584e
AleksisSanhes <oliveyrapolusmak>
2022-06-14 22:45:46 +0300

    work - the best

--- second_way.py
+++ second_way.py
@@ -1,37 +1,44 @@
 import pdfplumber
 from abbrevia import abbreviation
+from datetime import date  # для сравнения дат
+from openpyxl.writer.excel import save_workbook
+from openpyxl import *
 
 dic = {}
+page = 0
+input_date = "21.5.2022".split('.')  # сюда вводи дату, с какой счет вести
+input_date = date(int(input_date[2]), int(input_date[1]), int(input_date[0]))
 
 
-def in_dic(description):
-    if description in dic:
+def in_dic(descrip):
+    if descrip in dic:
         if sign == '-':
-            dic[description] -= float(amount)
+            dic[descrip] -= float(amount)
         else:
-            dic[description] += float(amount)
+            dic[descrip] += float(amount)
     else:
-        dic[description] = float(sign + amount)
+        dic[descrip] = float(sign + amount)
 
 
 with pdfplumber.open("15.03-30.05.pdf") as temp:
-    page = 0
     try:
         while temp.pages[page].extract_text():
-            first_page = temp.pages[page].extract_text()  # выбираем стр и вытаскиваем текст <class 'str'>
-            first_page = first_page.split('\n')  # <class 'list'>
+            first_page = temp.pages[page].extract_text()  # текст страницы <class 'str'>
+            first_page = first_page.split('\n')  # разделение по переносу срок <class 'list'>
             for i in first_page:
-                separated = i.split()
-                if separated[0][0].isdigit() and len(separated) > 9:
-                    date = separated[0]
-                    sign = separated[3]
-                    amount = separated[4] if separated[5][-1] != '₽' else separated[4] + separated[5]
-                    amount = amount.replace('₽', '').replace(',', '.')
-                    description = separated[8:] if not separated[8:][0][0].isdigit() else separated[9:]
-                    description = description if description[0] != '₽' else description[1:]
-                    description = ' '.join(description)
-                    description = abbreviation(description)
-                    in_dic(description)
+                separated = i.split()  # разделение по пробелам
+                if separated[0][0].isdigit() and len(separated) > 9:  # нужные строки с датами
+                    date_taken = separated[0]
+                    current_date = date(int(date_taken[6:10]), int(date_taken[3:5]), int(date_taken[0:2]))
+                    if input_date <= current_date:
+                        sign = separated[3]
+                        amount = separated[4] if separated[5][-1] != '₽' else separated[4] + separated[5]
+                        amount = amount.replace('₽', '').replace(',', '.')
+                        description = separated[8:] if not separated[8:][0][0].isdigit() else separated[9:]
+                        description = description if description[0] != '₽' else description[1:]
+                        description = ' '.join(description)
+                        description = abbreviation(description)
+                        in_dic(description)
             page += 1
     except IndexError:
         pass
@@ -39,8 +46,6 @@ with pdfplumber.open("15.03-30.05.pdf") as temp:
 
 # ---------------------Запись в файл--------------------------#
 
-from openpyxl.writer.excel import save_workbook
-from openpyxl import *
 
 FILE_NAME = 'excel.xlsx'
 
@@ -49,7 +54,7 @@ try:
 except:
     wb = Workbook()
 
-ws = wb.create_sheet('new')
+ws = wb.create_sheet(str(input_date) + ' - ' + str(current_date))
 
 num = 2
 _cell = ws['A1']
@@ -58,13 +63,13 @@ _cell2 = ws['B1']
 _cell.value = 'Описание'
 _cell2.value = 'Сумма'
 
-for descr, sum in dic.items():
+for descr, SUM in dic.items():
     _cell = ws['A' + str(num)]  # Определяем необходимую ячейку на листе
     _cell2 = ws['B' + str(num)]
 
     _cell.value = descr  # Пишем в ячейке
-    _cell2.value = sum
+    _cell2.value = SUM
     num += 1
-    print(descr, '->', sum)
+    # print(descr, '->', SUM)
 
 save_workbook(wb, FILE_NAME)
